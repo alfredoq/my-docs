bd97ca00ad1ac40c584a7ec0e6ac7d96bdb27c49
Fredy Quevedo <aquevedo@unc.edu.ar>
2025-06-10 21:01:01 -0300

    Add github actions to deploy workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://alfredoq.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'alfredoq', // Usually your GitHub org/user name.
+  projectName: 'my-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages', // The branch that GitHub pages will deploy from.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
